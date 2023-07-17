b8bd425242a4985cececc02518a775c2027a0a87
DivyaMishra6 <divyamishra599@gmail.com>
2023-07-17 23:43:28 +0530

    [Divya]: Write Some JS Programming

---
+++ Programming.js
@@ -0,0 +1,27 @@
+ var a = 10;
+ var b = 20;
+ var c = a+b;
+ console.log("Addition is" + c);
+
+
+ var d = Math.floor(Math.random()* 10) % 3;
+console.log(d)
+
+console.log("******************************");
+
+
+if (true)
+{
+var a = 10
+
+ //prints 10
+console.log(a)
+}
+console.log("******************************");
+
+let x = 15;
+let y = 40;
+let z= x + y;
+console.log("Add : " + z);
+
+
