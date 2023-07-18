1255475a2e4e75cc1591a4b175d0ca20bf7ef63c
DivyaMishra6 <divyamishra599@gmail.com>
2023-07-18 18:33:21 +0530

    [Divya]: Write Some JS Programming

--- Programming.js
+++ Programming.js
@@ -24,4 +24,50 @@ let y = 40;
 let z= x + y;
 console.log("Add : " + z);
 
+console.log("******************************");
+
+let arr = new Array();
+
+for(let i = 0; i < 10; i++){
+    arr[i] = Math.floor(Math.random() * (999-100+1) + 100);
+    console.log(arr[i]);
+}
+SecondLargestNumber(arr);
+SeconSmallestNumber(arr);
+
+
+
+function SecondLargestNumber(arr){
+    let largestNumber = arr[0];
+    let secondLargestNumber = arr[0];
+
+    for(let i = 0;i < arr.length ;i++){
+        if(arr[i] > largestNumber){
+            secondLargestNumber = largestNumber;
+            largestNumber = arr[i];
+        }
+        else if( arr[i] > secondLargestNumber){
+            secondLargestNumber = arr[i];
+        }
+    }
+    console.log("Second largest number is : "+secondLargestNumber);
+}
+
+
+function SeconSmallestNumber(arr){
+    let smallestNumber = arr[0];
+    let secondSmallestNumber = arr[0];
+
+    for(let i = 0;i <arr.length;i++){
+        if(arr[i] < smallestNumber){
+            secondSmallestNumber = smallestNumber;
+            smallestNumber = arr[i];
+        }
+        else if(arr[i] < secondSmallestNumber){
+            secondSmallestNumber = arr[i];
+        }
+    }
+    console.log("Second smallest number is : "+secondSmallestNumber);
+}
+
 
